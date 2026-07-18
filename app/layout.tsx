284d0d904bda53cf1e6574e9ffdaa3acc964242d
Erfan Sefat <erfansefat@outlook.com>
2026-07-18 12:45:38 +0330

    Make providers component & add it to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import { Geist, Geist_Mono } from "next/font/google";
 
+import Providers from "@/lib/providers";
+
 import type { Metadata } from "next";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,7 +31,9 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="flex min-h-full flex-col">{children}</body>
+      <body className="flex min-h-full flex-col">
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
